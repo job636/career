887c63ca9e15a66880428399e44f7a65c046f774
SSAC Counselor <job6366@gmail.com>
2026-04-28 20:01:57 +0900

    add test endpoint

---
+++ api/test.js
@@ -0,0 +1,6 @@
+export default async function handler(req, res) {
+    const apiKey = process.env.GEMINI_API_KEY;
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${apiKey}`);
+    const data = await response.json();
+    return res.status(200).json(data);
+}
